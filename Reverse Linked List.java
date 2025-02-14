d1c5407d3ba72334967db0d58e3aa81004521665
Sami <sami.daraghmeh1306@gmail.com>
2025-02-14 15:40:24 +0200

    Create Reverse Linked List.java

---
+++ Reverse Linked List.java
@@ -0,0 +1,19 @@
+import java.lang.classfile.components.ClassPrinter.ListNode;
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+}
+// Time: O(N)
+// Space: O(1)
