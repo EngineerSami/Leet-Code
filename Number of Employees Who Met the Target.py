a18f550363895f59075134a3b365fe28c15029f9
Sami <sami.daraghmeh1306@gmail.com>
2025-02-23 09:19:48 +0200

    Create Number of Employees Who Met the Target.py

---
+++ Number of Employees Who Met the Target.py
@@ -0,0 +1,5 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        return sum(1 for hour in hours if hour >= target)
+#Time complexity: O(n)
+#Space complexity: O(1)
